df66441dc6fe36fd15ca537dfa0912afab9c56f9
Nikolai Gavriushin <gna@pm.me>
2025-12-18 14:26:29 +0400

    create project structure

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 
 const { PORT = 3001 } = process.env;
 
 const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to the database.");
+  })
+  .catch(console.error);
 
 app.listen(PORT, () => {
   console.log(`Express is running on port ${PORT}...`);
